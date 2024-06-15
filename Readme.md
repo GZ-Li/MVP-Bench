9a549cdd835bc48a21368e9d02971d8918eb5f38
GZ-Li <109974803+GZ-Li@users.noreply.github.com>
2024-06-16 02:07:36 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# MVP-Bench: Can Large Vision-Language Models Conduct Multi-level Visual Perception Like Humans?
+
+This repository contains data and code for the paper [MVP-Bench: Can Large Vision-Language Models Conduct Multi-level Visual Perception Like Humans?](). 
+
+## ECHo Corpus
+
+Currently, we only release the code to crawl and preprocess the screenplays.
+To get selected frames and associated annotations, please fill out [this form](https://docs.google.com/forms/d/e/1FAIpQLScchFnDYqmCxtwe5GszhCTeM4MPs2HOAYPXeCbSgM5614_kiQ/viewform?vc=0&c=0&w=1&flr=0&usp=mail_form_link).
+Note that you must possess the _CSI: Crime Scene Investigation_ DVDs (seasons 1-8) for us to make the frames available for you.
+You can make the purchase on sites like Amazon and eBay.
---
+++ inference.py
@@ -0,0 +1,94 @@
+import os
+import json
+import codecs
+import jsonlines
+from tqdm import tqdm
+
+import torch
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+import sys
+from vlmeval.config import supported_VLM
+from vlmeval.utils import track_progress_rich
+from vlmeval.smp import *
+
+os.environ['OPENAI_API_KEY'] = "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"
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyCUHr41PVtRGVIfM9rkmQSCgROkx9rgh-M'
+
+def jsonline_load(fname):
+    with jsonlines.open(fname, mode='r') as reader:
+        data = [r for r in reader]
+    return data
+
+def jsonlines_dump(data, fname):
+    with jsonlines.open(fname, mode='w') as writer:
+        writer.write_all(data)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name",default='GPT4o',type=str)
+    parser.add_argument("--system_prompt", default='', type=str)
+    parser.add_argument("--img_dir", default='data/Images', type=str)
+    parser.add_argument("--output_dir", default='model_predictions', type=str)
+    parser.add_argument("--qas_pth", default='data/all_questions.json', type=str)
+    parser.add_argument("--mcq_pth", default='data/mcq_all_questions.json', type=str)
+    parser.add_argument("--question_type", default="all_questions", type=str) # all_questions/mcq_questions
+    args = parser.parse_args()
+
+    model_name = args.model_name
+    output_path = os.path.join(args.output_dir, f'{model_name}_supp.pkl')
+    if args.question_type == 'all_questions':
+        qas = jsonline_load(args.qas_pth)
+    elif args.question_type == 'mcq_questions':
+        qas = jsonline_load(args.mcq_pth)
+    else:
+        print("Incorrect Question Type or Question Path")
+    
+    system_prompt = args.system_prompt
+    if not system_prompt:
+        model = supported_VLM[model_name](system_prompt=system_prompt)
+    else:
+        model = supported_VLM[model_name]()
+    gen_func = model.generate
+    
+    res = {}
+    if os.path.exists(output_path):
+        res = load(output_path)
+    
+    indices = [struct['question_id'] for struct in qas if struct['question_id'] not in res]
+    if not system_prompt:
+        structs = [dict(message=[
+            {'type': 'image', 'value': os.path.join(args.img_dir, struct['image'])},
+            {'type': 'text', 'value': system_prompt + '\n' + struct['question']},
+        ], dataset='PerceptionBench') for struct in qas if struct['question_id'] not in res]
+    else:
+        structs = [dict(message=[
+            {'type': 'image', 'value': os.path.join(args.img_dir, struct['image'])},
+            {'type': 'text', 'value': struct['question']},
+        ], dataset='PerceptionBench') for struct in qas if struct['question_id'] not in res]
+    
+    if model_name in ['GPT4V', 'GPT4o']:
+        if len(structs):
+            track_progress_rich(gen_func, structs, nproc=4, chunksize=4, save=output_path, keys=indices)
+    else:
+        for i, struct in tqdm(zip(indices, structs)):
+            response = model.generate(**struct)
+            torch.cuda.empty_cache()
+            res[i] = response
+            if (len(res) + 1) % 20 == 0:
+                dump(res, output_path)
+        dump(res, output_path)
+    
+    res = load(output_path)
+    
+    res = {k: {'question_id': k, 'output': v} for k,v in res.items()}
+    for x in qas:
+        res[x['question_id']].update({
+            'prompt': x['question'],
+            'image': x['image'],
+            'model_id': model_name
+        })
+    jsonlines_dump(res.values(), os.path.join(OUTPUT_DIR, f'{model_name}.jsonl'))
+    jsonlines_dump(res.values(), os.path.join(OUTPUT_DIR, f'mcq_{model_name}.jsonl'))
+    os.remove(output_path)
